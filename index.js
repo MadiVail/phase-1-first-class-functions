124bb40f0368aceda37c1572431923a3f505adbc
Madisyn Vail <virtualmadi3@gmail.com>
2022-01-12 15:32:19 -0600

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function receivesAFunction(cb) {
+    cb();
+}
+
+function returnsANamedFunction() {
+    return function named() {
+
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    };
+}
